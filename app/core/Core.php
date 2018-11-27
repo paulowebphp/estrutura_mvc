d01fed8f0a3cf0ea819eaee7e60dccc3677b0af8
paulowebphp <paulowebphp@gmail.com>
2018-11-27 16:54:49 -0200

    Criando URL amigável na Classe Core método run; não consegui usar o parâmetro PHP_SELF de , e tive que adaptar utilizando o REQUEST_URI

--- app/core/Core.php
+++ app/core/Core.php
@@ -5,8 +5,17 @@ class Core
 
 	public function run()
 	{
+		$url = $_SERVER["REQUEST_URI"];
 
-		echo "estou executando o run";
+		if( $url != "/" )
+		{
+			$url = explode('/', $url);
+			# Remove primeiro item de um array
+			array_shift($url);
+		}#end if
+		
+		echo "<pre>";
+		print_r($url);
 
 	}#END run
 
