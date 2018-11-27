3940d3c34af9c7b7f0d96dd06587f744c1cce459
paulowebphp <paulowebphp@gmail.com>
2018-11-27 17:20:31 -0200

    Separando os elementos da url amigavel criando os métodos Getters dentro da classe Core e atribuindo seus valores dentro do método Run

--- app/core/Core.php
+++ app/core/Core.php
@@ -3,21 +3,67 @@
 class Core
 {
 
+	private $controller;
+	private $metodo;
+	private $parametros = array();
+
+
 	public function run()
 	{
 		$url = $_SERVER["REQUEST_URI"];
 
 		if( $url != "/" )
 		{
+			# colocando as partes da url num array
 			$url = explode('/', $url);
-			# Remove primeiro item de um array
+			
+			# Criando controller
+			# array_shift() remove primeiro item de um array
 			array_shift($url);
+			$this->controller = $url[0];
+			
+			# Criando Metodo
+			array_shift($url);
+			$this->metodo = $url[0];
+			
+			# Criando Parametros
+			array_shift($url);
+			# array_filter() pega só os valores VÁLIDOS de um array, ou seja, se tiver elementos vazios ele percebe automaticamente e forma um array com o que tiver no momento
+			$this->parametros = array_filter($url);
+
+
+
 		}#end if
 		
-		echo "<pre>";
-		print_r($url);
+		//echo "<pre>";
+		//print_r($url);
 
 	}#END run
+
+
+
+	public function getController()
+	{
+		return $this->controller;
+
+	}#END getController
+
+
+
+	public function getMetodo()
+	{
+		return $this->metodo;
+
+	}#END getMetodo
+
+
+
+	public function getParametros()
+	{
+		return $this->parametros;
+
+	}#END getParametros
+
 
 
 }#END class Core
--- index.php
+++ index.php
@@ -4,5 +4,16 @@ require 'app/core/Core.php';
 $core = new Core;
 $core->run();
 
+echo "controller: ". $core->getController();
+echo "<br>";
+echo "metodo: ". $core->getMetodo();
+echo "<br>";
+$parametros = $core->getParametros();
+foreach ($parametros as $param) {
+	# code...
+	echo "parametro: ".$param;
+	echo "<br>";
+}
+
 
  ?>
